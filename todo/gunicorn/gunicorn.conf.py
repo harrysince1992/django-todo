871afafca16eea3c09485f03caed02958a6e9d8e
harrysince1992 <42317668+harrysince1992@users.noreply.github.com>
2021-08-14 21:35:33 +0530

    Update gunicorn.conf.py

--- todo/gunicorn/gunicorn.conf.py
+++ todo/gunicorn/gunicorn.conf.py
@@ -9,4 +9,4 @@ errorlog = "/var/log/gunicorn.error.log"
 # Whether to send Django output to the error log 
 capture_output = True
 # How verbose the Gunicorn error logs should be 
-loglevel = "info"
+loglevel = "debug"
