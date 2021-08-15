a3be4de978f1fdf35bd47282de53b48a4758d367
harrysince1992 <42317668+harrysince1992@users.noreply.github.com>
2021-08-15 09:26:38 -0400

    Update and rename install.sh to install_app.sh

--- install_app.sh
+++ install_app.sh
@@ -2,13 +2,14 @@
 #Sample shell file to deploy app automatically using docker swarm and stack
 #Author: Harpreet Singh
 #Save script output to a file
-#current_date=$(date)
-#if [ -d /opt/script-log/ ]
-#then	
-#	bash | tee /opt/script-log/install.log_${current_date}  2>&1 #this will save all output including errors until the bash session is ended
-#else
-#	mkdir -p /opt/script-log/
-#fi
+current=$(date +%s)
+if [ -d /opt/script-log/ ]
+then	
+	bash | tee /opt/script-log/install.log_${current}  2>&1 #this will save all output including errors until the bash session is ended
+else
+	mkdir -p /opt/script-log/
+	bash | tee /opt/script-log/install.log-${current}
+fi
 
 
 #color definitions
@@ -25,7 +26,6 @@ sudo mv /home/ubuntu/code.tar.gz /opt/prod/
 
 # Unpack the tar file
 cd /opt/prod/
-current=$(date +%s)
 tar -xvzf /opt/prod/code.tar.gz
 
 # After unpacking, save tar file as backup to another folder
@@ -33,7 +33,6 @@ sudo mv code.tar.gz ../backup/code.tar.gz_${current}
 
 # New image has to be created for application
 cd /opt/prod/todo/
-
 echo -e "${GREEN}Building new docker image for the application...${NC}"
 sudo docker build -t django-img .
 
@@ -54,14 +53,13 @@ if [ $? -eq 0 ]
 then
         echo -e "${GREEN} Image has been pushed to private docker repo.${NC}"
 else
-	echo -e "${RED} Image push failed.${NC}"
+	echo -e "${RED} Image push failed. Please check logs in /opt/script-log/ directory.${NC}"
 fi
 
 # after image is pushed, deploy new stack
 cd ../
-sudo chown -R root:root /opt/prod/filebeat/filebeat.yml
-sudo docker stack deploy --with-registry-auth -c docker-compose.yml webapp
 
+sudo docker stack deploy --with-registry-auth -c docker-compose.yml webapp
 
 if [ $? -eq 0 ]
 then
@@ -70,5 +68,5 @@ else
         echo -e "${RED}App deployment  was unsuccessful.${NC}"
 fi
 
-
+######################################################################################################
 
